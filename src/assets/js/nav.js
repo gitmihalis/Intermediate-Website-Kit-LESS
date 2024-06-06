c173c9f72c42a0adec568e36685db4bfa099395a
Mihalis <mihaliswastaken@protonmail.com>
2024-06-06 11:36:45 -0400

    add nav and hero

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,41 +1,47 @@
 // add classes for mobile navigation toggling
-var CSbody = document.querySelector('body');
-const CSnavbarMenu = document.querySelector('#cs-navigation');
-const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+var CSbody = document.querySelector("body");
+const CSnavbarMenu = document.querySelector("#cs-navigation");
+const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 
-// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
-function ariaExpanded(element) {
-    const isExpanded = element.getAttribute('aria-expanded');
-    if (isExpanded === "false") {
-        element.setAttribute('aria-expanded', 'true');
-    } else {
-        element.setAttribute('aria-expanded', 'false');
-    };
-};
-
-function toggleMenu() {
-    CShamburgerMenu.classList.toggle('cs-active');
-    CSnavbarMenu.classList.toggle('cs-active');
-    CSbody.classList.toggle('cs-open');
+CShamburgerMenu.addEventListener('click', function() {
+    CShamburgerMenu.classList.toggle("cs-active");
+    CSnavbarMenu.classList.toggle("cs-active");
+    CSbody.classList.toggle("cs-open");
     // run the function to check the aria-expanded value
-    ariaExpanded(CShamburgerMenu);
-}
-
-const dropdownElements = document.querySelectorAll(".cs-dropdown");
-dropdownElements.forEach(element => {
-    element.addEventListener("focusin", function (event) {
-        ariaExpanded(element);
-    });
-
-    element.addEventListener("focusout", function (event) {
-        ariaExpanded(element);
-    });
+    ariaExpanded();
 });
 
-CShamburgerMenu.addEventListener('click', toggleMenu);
-document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape" && CShamburgerMenu.classList.contains("cs-active")) {
-        toggleMenu();
-    }
-})
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
+function ariaExpanded() {
+    const csUL = document.querySelector('#cs-expanded');
+    const csExpanded = csUL.getAttribute('aria-expanded');
 
+    if (csExpanded === 'false') {
+        csUL.setAttribute('aria-expanded', 'true');
+    } else {
+        csUL.setAttribute('aria-expanded', 'false');
+    }
+}
+
+// This script adds a class to the body after scrolling 100px
+// and we used these body.scroll styles to create some on scroll 
+// animations with the navbar
+
+document.addEventListener('scroll', (e) => { 
+    const scroll = document.documentElement.scrollTop;
+    if(scroll >= 100){
+document.querySelector('body').classList.add('scroll')
+    } else {
+    document.querySelector('body').classList.remove('scroll')
+    }
+});
+
+// mobile nav toggle code
+const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+    for (const item of dropDowns) {
+        const onClick = () => {
+        item.classList.toggle('cs-active')
+    }
+    item.addEventListener('click', onClick)
+    }
+                            
